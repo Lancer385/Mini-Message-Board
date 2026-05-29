e9c280525473fc6ecc8777361373a867f9ea3014
Lancer385 <ahmedkhalaf1234123@gmail.com>
2026-05-29 05:05:42 +0300

    add a button to view messages for each user in separate page

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const createError = require('http-errors');
 const router = express.Router();
 
 
 const messages = [
   {
+    id: 0,
     text: "hello?",
-    user: "Lancer",
+    user: "lancer",
     date: new Date()
   },
 
   {
+    id: 1 ,
     text: "hiiiiiii",
-    user: "Radish",
+    user: "radish",
     date: new Date()
   }
 
@@ -28,9 +31,19 @@ router.get('/new', function(req, res) {
 
 router.post('/new', function(req, res) {
     const {name, message} = req.body
-    messages.push({text: message, user: name, date: new Date()})
+    messages.push({id: messages.length, text: message, user: name, date: new Date()})
 
   res.redirect('/');
 });
+
+router.get('/message/:id', (req, res, next) => {
+    for (let message of messages){
+      if (message.id === Number(req.params.id)){
+        res.render("message", {message: message})
+        return;
+      }
+    }
+    next(createError(404))
+})
 
 module.exports = router;
